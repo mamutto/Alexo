09b0f8e112466638180cfecab967f6f215911bb2
Са <kuhajobs@gmail.com>
2016-09-04 14:14:15 +0300

    Calculate change

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,5 +1,14 @@
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("Hello!!!!");
+        double first = 10.3;
+        int second = (int) ( 10 - first);
+        short third = (short) (first * second);
+        int four = (int) (first / second);
+        double five = Math.pow(first,2);
+        System.out.println("1+2 " + first + second);
+        System.out.println("1-2 " + second);
+        System.out.println("1*2 " + third );
+        System.out.println("1/2 " + four);
+        System.out.println("1 in pow " + five);
     }
 }
